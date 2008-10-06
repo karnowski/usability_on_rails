85c7408c2b2c3b70260c30c21ee7f62f8a32ad7c
Lawrence Karnowski <larry@thinkrelevance.com>
2008-10-05 22:23:35 -0400

    db schema

--- stakechart/db/schema.rb
+++ stakechart/db/schema.rb
@@ -9,6 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20081006020133) do
+
+  create_table "bogeys", :force => true do |t|
+    t.string   "name"
+    t.string   "living_name"
+    t.string   "classification"
+    t.string   "status"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
